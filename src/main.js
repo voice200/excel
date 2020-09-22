8988bd27ee74d58be935d981993c3346b83fa74c
voice200 <voice200rabota@gmail.com>
2020-09-22 21:29:18 +0300

    "Finish base frawemork functional"

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import {Header} from '@/component/header/Header';
 import {ToolBar} from '@/component/toolBar/ToolBar';
 import {Formula} from '@/component/formula/Formula';
 import {Table} from '@/component/table/Table';
-const excel = new Excel('#app'{
+const excel = new Excel('#app',{
     components: [Header, ToolBar, Formula, Table]
 });
 
