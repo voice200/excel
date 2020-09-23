446c55801f961e13f6ad288344ad377333f75416
voice200 <voice200rabota@gmail.com>
2020-09-23 21:18:54 +0300

    "Finish resize table"

--- src/component/table/Table.js
+++ src/component/table/Table.js
@@ -1,9 +1,26 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/component/table/table.template';
+import {$} from '@core/dom';
+import {tableResize} from '@/component/table/table.resize';
+import {shouldResize} from '@/component/table/table.function';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table';
+
+    constructor($root) {
+        super($root, {
+            listeners: ['mousedown']
+        });
+    }
+
     toHTML() {
         return createTable(40);
     }
+
+    onMousedown(event) {
+        if (shouldResize(event)) {
+            tableResize(this.$root, event);
+        }
+    }
 }
+
